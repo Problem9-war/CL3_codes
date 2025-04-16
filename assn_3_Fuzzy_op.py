7be05d133cf9b514da1eca749576d21a391575b6
Problem9-war <problemwar9@gmail.com>
2025-04-16 20:33:54 +0530

    Add files via upload

---
+++ assn_3_Fuzzy_op.py
@@ -0,0 +1,67 @@
+#  Create 2 dictionaries representing 2 sets a & b and there elements
+a = {'x1':0.2,'x2':0.6,'x3':0.3,'x4':0.5,'x5':0.9}
+b = {'x1':0.4,'x2':0.7,'x3':0.5,'x4':0.1,'x5':0.2}
+
+# Union operation -> Return max value by comapring corresponding elements from 2 sets
+def fuzzy_union(a,b):
+    union ={}
+    for key in a.keys():
+        union[key] = max(a[key],b[key])
+    return union
+print("Union :\n",fuzzy_union(a,b))
+
+
+# Intersection Operation -> return min value by comparing .....
+def fuzzy_intersection(a,b):
+    intersection = {}
+    for key in a.keys():
+        intersection[key] = min(a[key],b[key])
+    return intersection
+print("\nIntersection:\n",fuzzy_intersection(a,b))
+
+# Complement Operation
+def complement(a): 
+    comp_a ={}
+    for key in a.keys():
+        comp_a[key] = 1- a[key]
+    return comp_a
+print("\nComplement :\n",complement(a))
+
+# Difference operation for Fuzzy set -> min(a[key],1-b[key])
+def diff(a,b):
+    diff ={}
+    for key in a.keys():
+        diff[key] = min(a[key],1-b[key])
+    return diff
+print("\nDifference : \n",diff(a,b))
+
+
+a = {'x1': 0.2, 'x2': 0.6}
+b = {'y1': 0.5, 'y2': 0.8}
+# Catersian Product -> same as intersection but here are 2 variables -> x,y (x and y variables have different values )instead of only x in above case of intersection
+def cart_product(a,b):
+    prod = {}
+    for a_key in a.keys():
+        for b_key in b.keys():
+            prod[(a_key,b_key)] = min(a[a_key],b[b_key])
+    return prod
+print("\nProduct : \n",cart_product(a,b))
+
+# Create 2 fuzzy relations
+# R: from X → Y
+R = {('x1', 'y1'): 0.2,('x1', 'y2'): 0.4,('x2', 'y1'): 0.6,('x2', 'y2'): 0.8}
+
+# S: from Y → Z
+S = {('y1', 'z1'): 0.3,('y1', 'z2'): 0.5,('y2', 'z1'): 0.7,('y2', 'z2'): 0.9}
+
+# min-max composition
+def max_min_composition(R, S):
+    result = {}
+    for x, y1 in R:
+        for y2, z in S:
+            if y1 == y2:
+                pair = (x, z)
+                value = min(R[(x, y1)], S[(y2, z)])
+                result[pair] = max(result.get(pair, 0), value)
+    return result
+print("\nMin Max Composition : \n",max_min_composition(R,S))
---
+++ assn_5_spray_drying.py
@@ -0,0 +1,52 @@
+# Importing Libraries
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPRegressor
+from geneticalgorithm import geneticalgorithm as ga
+
+# Fitness func
+def fitness_value(params):
+    hidden_layer = (int(params[0]),) * int(params[1])
+    activation=['identity','logistic','tanh','relu'][int(params[2])]
+    solver=['lbfgs','sgd','adam'][int(params[3])]
+    model = MLPRegressor(hidden_layer_sizes=hidden_layer,activation=activation,solver=solver)
+    model.fit(x_train,y_train)
+    fitness = -model.score(x_val,y_val)
+    return fitness
+
+# Final nn 
+def train_neural_network(params):
+    hidden_layer = (int(params[0]),) * int(params[1])
+    activation=['identity','logistic','tanh','relu'][int(params[2])]
+    solver=['lbfgs','sgd','adam'][int(params[3])]
+    model = MLPRegressor(hidden_layer_sizes=hidden_layer,activation=activation,solver=solver)
+    model.fit(x_train,y_train)
+    validation_error = -model.score(x_val,y_val)
+    return validation_error
+    
+# Synthetic DS
+np.random.seed(2)
+num_samples=1000
+num_features=5
+x = np.random.rand(num_samples,num_features)
+true_coefficient = np.random.rand(num_features)*10
+noise = np.random.normal(loc=0,scale=1,size=num_samples)
+y = np.dot(x,true_coefficient) + noise
+x_train,x_val,y_train,y_val = train_test_split(x,y,test_size=0.2,random_state=3)
+
+# Config GA
+no_of_param =4
+varbound = np.array([[5,50],[1,5],[0,3],[0,2]])
+algorithm_param = {'max_num_iteration': 1, 'population_size': 100, 'mutation_probability': 0.1,
+                   'elit_ratio': 0.01, 'crossover_probability': 0.5, 'parents_portion': 0.3,
+                   'crossover_type': 'uniform', 'max_iteration_without_improv': None}
+model = ga(function=fitness_value,dimension=no_of_param,variable_type='int',variable_boundaries=varbound,algorithm_parameters=algorithm_param)
+
+# Run GA Model
+model.run()
+
+# Retrieve and save best params
+best_params=model.output_dict['variable']
+validation = train_neural_network(best_params)
+print("best parameters found by GA : ",best_params)
+print("Valiation error of Neural Network ",validation)
